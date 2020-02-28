97ad241fb48af6728bfbef6ba7977e4a69978c6b
Brian Bouchard <b.bouch@gmail.com>
2020-02-27 19:32:35 -0700

    add api endpoint framework

--- server/routes.js
+++ server/routes.js
@@ -4,24 +4,24 @@ const router = express.Router();
 /* --------------------- Get --------------------- */
 // activities
 router.getAllActivities('/activities', function (req, res) {
-  qQueries.getProductQuestions()
+  
     // .then((results) => res.status(200).send(results))
 })
 
 router.getActivity('/activity', function (req, res) {
-  qQueries.getActivity()
+
     // .then((results) => res.status(200).send(results))
 })
 
 /* --------------------- Add --------------------- */
 router.addActivity('/addActivity', function (req, res) {
-  qQueries.addActivity()
+
     // .then((results) => res.status(200).send(results))
 })
 
 /* --------------------- Delete --------------------- */
 router.getActivity('/deleteActivity', function (req, res) {
-  qQueries.getActivity()
+
     // .then((results) => res.status(200).send(results))
 })
 
