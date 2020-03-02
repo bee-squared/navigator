c64fabbf7266aa39ccf70a39392a08226e086fba
Brian Bouchard <b.bouch@gmail.com>
2020-03-02 13:25:04 -0700

    add all fields to new activiy form and style

--- src/components/activity/new-activity/new-activity.jsx
+++ src/components/activity/new-activity/new-activity.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import propTypes from 'prop-types';
 import './new-activity.scss';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
 
 class NewActivity extends React.Component {
@@ -12,7 +13,14 @@ class NewActivity extends React.Component {
   }
 
   render () {
+
+    const style = {
+      color: '#ffffff',
+      backgroundColor: '#336699',
+    };
+
     return (
+
       <div className="newActivityContainer">
         <div className="newActivityPageHeader">Add a New Activity</div>
         <div className="newActivityBody">
@@ -21,7 +29,7 @@ class NewActivity extends React.Component {
             <input className="textInput" type="text" name="title"/>
             <div className="inputBlockContainer">
               <div className="sportContainer">
-                <div className="activityInputTitle">Sport</div>
+                <div className="inputSportHeader">Sport</div>
                   <select id="sport" class="DropDownInput" name="sport">
                     <option value="none">Select a Sport</option>
                     <option value="Climb">Climb</option>
@@ -53,15 +61,28 @@ class NewActivity extends React.Component {
                 <input className="textInput" type="text" name="elevation" placeholder="Elevation in miles"/>
               </div>
             </div>
+            <div className="inputHeader">Location</div>
+            <input className="textInput" type="text" name="location"/>
+            <div className="inputHeader">Description</div>
+            <textarea className="textAreaInput" type="text" name="description"/>
+            <div className="inputHeader">Rating</div>
+            <select id="activityRating" name="rating">
+              <option class="ratingOption" value="none">Select a Rating</option>
+              <option class="ratingOption" value="5">5 - Glorious</option>
+              <option class="ratingOption" value="4">4 - Memorable</option>
+              <option class="ratingOption" value="3">3 - Meh</option>
+              <option class="ratingOption" value="2">2 - Just Okay</option>
+              <option class="ratingOption" value="1">1 - Never Again</option>
+            </select>
           </form>
+          <div className="buttonContainer">
+            <Button style={style} variant="text">Add Activity</Button>
+            <button type="text" className="cancelButton">Cancel</button>
+          </div>
         </div>
       </div>
     );
   }
-}
-
-NewActivity.propTypes = {
-
 }
 
 export default NewActivity;
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import * as Colors from '@material-ui/core/colors';
 import './navbar.scss';
 
 const useStyles = makeStyles(theme => ({
