1be125bf6e2f626442ed7e4e7ff20f5862181de6
Brian Bouchard <b.bouch@gmail.com>
2020-03-10 01:38:45 -0600

    update name of server file to app for heroku

--- database/index.js
+++ database/index.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true });
+const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost/users';
+
+mongoose
+  .connect(CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 
