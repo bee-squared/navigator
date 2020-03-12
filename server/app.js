985dd0d600c7b03dd13d666f0358607eb4997657
Brian Bouchard <b.bouch@gmail.com>
2020-03-11 21:34:36 -0600

    require node path module

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes.js');
 const PORT = process.env.PORT || 3000;
 
@@ -12,5 +13,9 @@ app.use(bodyParser.json());
 
 app.use('/', routes);
 app.use(express.static('build'));
+
+app.get('*', (req,res) =>{
+	res.sendFile(path.join(__dirname+'/build/index.html'));
+});
 
 app.listen(PORT, () => {console.log(`The server is up and running on port: ${PORT}`)});
